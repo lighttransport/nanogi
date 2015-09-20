7740e245f74e5b1025edb856fa21e15e6c015995
Syoyo Fujita <syoyo@lighttransport.com>
2015-09-20 16:36:00 +0900

    Update doc.

--- utils/runc/setup.sh
+++ utils/runc/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# rm -rf nanogi-fs
-# mkdir nanogi-fs
-# tar -C nanogi-fs -jxf ../nanogi-dist.tar.bz2
+rm -rf nanogi-fs
+mkdir nanogi-fs
+tar -C nanogi-fs -jxf ../nanogi-dist.tar.bz2
 
 node setup.js input.config.json input.runtime.json $(id -u) $(id -g)
